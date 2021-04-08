0eb06d38a2580c6e9ee9de04d98010ef63dae6a0
equlon <81644460+equlon@users.noreply.github.com>
2021-04-08 13:23:36 +0200

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -1,17 +1,1 @@
-const Command = require('../structures/Command');
-
-module.exports = class extends (
-  Command
-) {
-  constructor(...args) {
-    super(...args, {
-      description: 'A command used for testing.',
-      aliases: ['test2', 'test3'],
-      enabled: true
-    });
-  }
-
-  run(message) {
-    message.channel.send('It works!');
-  }
-};
+Ide gas 
